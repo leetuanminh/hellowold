ca8bb8151d04970710d792e8d67a83eac61bc511
leetuanminh <leeminhtuanoo@gmail.com>
2021-09-24 11:49:58 +0700

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("mot voi mot la hai \n");
+	printf("hai nhan hai la bon \n");
+	printf("bon voi mot la nam \n");
+	printf("nam voi nam la muoi \n");
+	printf("hehe\n");
+		
+// if(true)
+//   printf("");
+//   }	
+	return 0;
+}
